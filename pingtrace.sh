be7c8f442cb40d9a30d96cd3bb87925f03f96f87
muokicaleb <muokicaleb@gmail.com>
2018-09-06 18:46:41 +0300

    script to identify when pinged

--- pingtrace.sh
+++ pingtrace.sh
@@ -4,4 +4,5 @@ if (( $EUID != 0 )); then
     echo "You need to run script as root" 
     exit
 fi
+# change wlo1 to name of your network interface
 tcpdump -i wlo1 icmp && icmp[icmptype]=icmp-echo -n ; echo "service is running"
