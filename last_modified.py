0a1bed38f9f9759bbf7136f57c21c69ca306cff5
muokicaleb <muokicaleb@gmail.com>
2018-09-15 19:15:55 +0300

    find the last date a website was modified

--- last_modified.py
+++ last_modified.py
@@ -1,5 +1,4 @@
 import requests
-import sys
 from urlparse import urlparse
 
 # This will return last modified time of given web page url
@@ -11,7 +10,7 @@ def get_last_modified(url):
         header = requests.head(url).headers
         if 'Last-Modified' in header:
             return header['Last-Modified']
-        print "Data is not available"
+        print ("Data is not available")
         return -1
     else:
         return -1
@@ -20,7 +19,8 @@ def get_last_modified(url):
 # Demonstration of function call
 if __name__ == '__main__':
     try:
-        url = sys.argv[1]
-        print get_last_modified(url)
+    	# enter the website 
+        url = ""
+        print (get_last_modified(url))
     except Exception as e:
         print ("Please provide proper url as a first argument.")
