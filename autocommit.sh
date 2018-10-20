c205cbeec498496a6b12040eed55eac1cfa6da6c
muokicaleb <muokicaleb@gmail.com>
2018-10-20 21:49:30 +0300

    auto commit on save

--- autocommit.sh
+++ autocommit.sh
@@ -1,4 +1,4 @@
 
 # a script that automatically commits when the file is saved
 # you need to install inotify-tools
-inotifywait -q -m -e CLOSE_WRITE --format="git commit -m 'autocommit on change' %w" file.txt | sh
+inotifywait -q -m -e CLOSE_WRITE --format="git commit -m 'autocommit on change' %w" *.* | sh
